5729a71ba4ea6b4ed290629698a6d11521276fac
MiyuSatoh <1811140013h@ed.fuk.kindai.ac.jp>
2020-10-27 13:50:15 +0000

    やっとできた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  #get 'users/index'
-  #get 'users/new'
-  #get 'users/create'
-  #get 'users/destroy'
+  get 'top/login_form'
+  post 'top/login'
+  get 'top/logout'
   resources :users
-  root 'users#index'
+  resources :tweets
+  #resources :likes
+  root 'tweets#index'
+  resources :likes
+  #resources :likes do
+   #post 'add' => 'likes#create'
+   #delete '/add' => 'likes#destroy'
+  #end
+  #post "likes/:id/destroy" => "likes#destroy"
+  delete 'likes/:id', to: 'likes#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
